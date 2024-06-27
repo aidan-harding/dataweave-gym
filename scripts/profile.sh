6d8358b770205b5814e23982da36311790242181
Aidan Harding <aidan.harding@nebulaconsulting.co.uk>
2024-06-27 15:01:21 +0100

    Add shell script and Apex for profiling

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,14 +1,19 @@
 #!/bin/zsh
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <parameter> <iterations> <output_file>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <apex_file> <start_size> <end_size> <step_size> <iterations> <output_file>"
     exit 1
 fi
 
-parameter=$1
-iterations=$2
-output_file=$3
+apex_file=$1
+start_size=$2
+end_size=$3
+step_size=$4
+iterations=$5
+output_file=$6
 
-for i in $(seq 1 "$iterations"); do
-    sf apex run --file "$parameter" | sed -rn 's/.*USER_DEBUG\|\[[0-9]+\]\|DEBUG\|(.*)/\1/p' >> "$output_file" 
+for ((size=start_size; size <= end_size; size += step_size)); do
+  for i in $(seq 1 "$iterations"); do
+      echo "Integer size=$size;" | cat - "$apex_file" | sf apex run | sed -rn 's/.*USER_DEBUG\|\[[0-9]+\]\|DEBUG\|(.*)/\1/p' >> "$output_file" 
+  done
 done
